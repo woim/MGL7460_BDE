08efcfa118c2624f302101e160760e78a6b0f706
francois <francoishebert@hotmail.fr>
2016-10-17 22:27:09 -0400

    Mis a jour convention du format sigle des cours

--- bde/test_acceptation/liste_test.rb
+++ bde/test_acceptation/liste_test.rb
@@ -3,9 +3,9 @@ require 'bde'
 
 # Cree une base de donnee locale pour test d'acceptation
 base_donnee_test = [
-"MATH20/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17",
-"CHIM45/Loiseau,Martin=14,10,15|Thibodeau,Jean,Charles-Henri=17,13,15",
-"PHI98/Loiseau,Martin=17,19,15|Thibodeau,Jean,Charles-Henri=18,19,16" ]
+"MATH002/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17",
+"CHIM005/Loiseau,Martin=14,10,15|Thibodeau,Jean,Charles-Henri=17,13,15",
+"PHI012/Loiseau,Martin=17,19,15|Thibodeau,Jean,Charles-Henri=18,19,16" ]
 nom_fichier = ".bde.txt"
 creer_base_donnee( base_donnee_test, nom_fichier )
 
@@ -13,20 +13,20 @@ describe Bde do
   describe "liste" do
   
     it "liste les classes" do
-      bde_cli( 'liste' ).must_equal ['MATH20','CHIM45','PHI98']
+      bde_cli( 'liste' ).must_equal ['MATH002','CHIM005','PHI012']
     end
     
     it "liste les classes dans l'ordre alphabetique" do
-      bde_cli( 'liste -alphabetique' ).must_equal ['CHIM45','MATH20','PHI98']
+      bde_cli( 'liste -alphabetique' ).must_equal ['CHIM005','MATH002','PHI012']
     end
     
     it "liste les etudiants de la classe MATH20" do
-      bde_cli( '--class=MATH20 liste' ).
+      bde_cli( '--class=MATH002 liste' ).
         must_equal ['Jean Charles-Henri Thibodeau', 'Martin Loiseau']
     end
     
     it "liste les etudiants de la classe MATH00 dans l'ordre alphabetique" do
-      bde_cli( '--class=MATH20 liste -alphabetique' ).
+      bde_cli( '--class=MATH002 liste -alphabetique' ).
         must_equal ['Martin Loiseau', 'Jean Charles-Henri Thibodeau']
     end
     
