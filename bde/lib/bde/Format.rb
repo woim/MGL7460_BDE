9687bd438a8e87a39b70cc880ac713c7e9c3e66c
francois <francoishebert@hotmail.fr>
2016-10-20 00:11:05 -0400

    Test unitaire class Format

--- bde/lib/bde/Format.rb
+++ bde/lib/bde/Format.rb
@@ -1,11 +1,22 @@
 class Format
-
-  def self.extraire_information( champ )
- 
-  end
+  def extraire_information( ligne )
+    champ = ligne.split("/")    
+    cours = Cours.new( champ[0] )
+    etudiants_information = champ[1].split("|")
+    etudiants_information.each do |info|
+      nom_info = info.split("=")[0]
+      nom = nom_info.split(",")[0]
+      prenoms = nom_info.split(",")[1..-1]
+      
+      notes_info = info.split("=")[1]
+      notes = notes_info.split(",").to_f    
+      
+      etudiant = Etudiant(nom,prenoms)
+      etudiant.ajouter_note( notes )
+      cours.ajouter_etudiant(etudiant)
+    end  
+  end  
+  def ecrire_information( cours )
   
-  def self.ecrire_information( cours )
-  
-  end
-  
+  end  
 end
---
+++ bde/test/test_format.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+require 'bde'
+
+
+
+describe Format do
+  before {     
+    etudiant1 = Etudiant.new( "Loiseau", "Martin" )
+    etudiant1.ajouter_note( [12,13,14] )
+    etudiant2 = Etudiant.new( "Thibodeau", "Jean", "Charles-Henri" )
+    etudiant2.ajouter_note( [18,18,17] )
+        
+    @cours = Cours.new( "MATH002" )
+    @cours.ajouter_etudiant( etudiant1 )
+    @cours.ajouter_etudiant( etudiant2 )
+    @champ = "MATH002/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17" 
+    
+    @format = Format.new
+  }
+    
+  describe "#extraire_cours_information" do  
+    it "extrait l'information d'une ligne" do
+      cours_extrait = @format.extraire_information( @champ )
+      @cours.must_equal( cours_extrait )
+    end
+  end
+  
+  describe "#ecrire_cours_information" do
+    it "ecrit l' information d'un cours dans un champ(string)" do
+      champ_ecrit = @format.ecrire_information( @cours )
+      @champ.must_equal( champ_ecrit )
+    end
+  end  
+end
