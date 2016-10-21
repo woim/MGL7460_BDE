95a69df8563ed31b61398a7d4155ba7675d302a2
francois <francoishebert@hotmail.fr>
2016-10-20 23:29:12 -0400

    Modification suite au discussion en cours

--- bde/lib/bde/BaseDonneeCours.rb
+++ bde/lib/bde/BaseDonneeCours.rb
@@ -1,8 +1,6 @@
 class BdCours
   attr_reader :cours
   def initialize( accesseur )
-    #~ fail "L'argument n'est pas de type Accesseur" \
-      #~ unless accesseur.instance_of? accesseur
     @cours = []
     @accesseur = accesseur
   end  
@@ -13,22 +11,15 @@ class BdCours
     
   end
   def charger_base_donnee( nom_fichier )
-    #~ fail "Un accesseur doit etre assigne avant de charger une base de 
-          #~ donnee" if @accesseur == nil
     @cours = @accesseur.charger_base_donnee( nom_fichier )
   end
   def ajouter_cours(sigle_cours)
       puts "ajout cours"
   end
   def selectionner_cours(sigle_cours)
-    fail "Le nom du cours doit etre de format SSSDDD" \
-      unless sigle_cours =~ /[A-Z][A-Z][A-Z]\d\d\d/
     index_cours = @cours.find_index{ |c| c.sigle.to_s == sigle_cours }
-    if index_cours != nil
-      return @cours[index_cours] 
-    else
-      return nil
-    end
+    res = nil
+    res = @cours[index_cours] if index_cours != nil
   end
   def lister_cours()
     liste = String.new
--- bde/test/test_baseDonneeCours.rb
+++ bde/test/test_baseDonneeCours.rb
@@ -4,49 +4,38 @@ require 'bde'
 nom_fichier = "bde_test.txt"
 
 describe BdCours do
+  let(:liste_cours ) { [Cours.new( "MAT002"), \
+                        Cours.new( "CHI005"), \
+                        Cours.new( "PHI012" )] }
+  before do
+    @mock_accesseur = MiniTest::Mock.new
+    @mock_accesseur.expect( :charger_base_donnee, liste_cours, [nom_fichier] )
+    @bde = BdCours.new( @mock_accesseur )
+  end
+
   describe ".new" do
     it "souleve une erreur si aucun argument n'est donne" do
       lambda{ BdCours.new }.must_raise( ArgumentError )
     end
-    
-    #~ it "souleve une erreur si l'argument n'est pas de type Accesseur" do
-      #~ lambda{ BdCours.new( "blah" ) }.must_raise( RunTimeError )
-    #~ end
   end 
 
   describe "#charger_base_donnee" do
-    let(:liste_cours ) { [Cours.new( "MATH002"), \
-                          Cours.new( "CHIM005"), \
-                          Cours.new( "PHI012" )] }
-    before{ 
-      mock_accesseur = MiniTest::Mock.new
-      mock_accesseur.expect( :charger_base_donnee, \
-                              liste_cours, \
-                              [nom_fichier] )
-      @bde = BdCours.new( mock_accesseur ) }
-    
-    it "souleve une erreur si le fichier n'existe pas" do
-      lambda{ @bde.charger_base_donnee( "blabla" ) }.
-        must_raise( RuntimeError )
-    end
-   
     it "charge une base de donnee" do
       @bde.charger_base_donnee( nom_fichier )
       @bde.cours.must_equal( liste_cours )
-      # ~mock_accesseur.verify
+      @mock_accesseur.verify
     end  
   end
   
   describe "#lister_cours" do 
-    let(:liste_cours ) { [Cours.new( "MATH002"), \
-                          Cours.new( "CHIM005"), \
+    let(:liste_cours ) { [Cours.new( "MAT002"), \
+                          Cours.new( "CHI005"), \
                           Cours.new( "PHI012" )] }
-    before{ 
-      mock_accesseur = MiniTest::Mock.new
-      mock_accesseur.expect( :charger_base_donnee, \
-                              liste_cours, \
-                              [nom_fichier] )
-      @bde = BdCours.new( mock_accesseur ) }
+    before do 
+      @mock_accesseur = MiniTest::Mock.new
+      @mock_accesseur.expect( :charger_base_donnee, liste_cours, [nom_fichier] )
+      @bde = BdCours.new( @mock_accesseur )
+    end
     
     it "retourne une chaine de caractere vide si la base de donnee
         n'a pas ete charge" do
@@ -55,39 +44,27 @@ describe BdCours do
     
     it "retourne la liste des sigles" do
       @bde.charger_base_donnee( nom_fichier )
-      @bde.lister_cours.must_equal "MATH002\nCHIM005\nPHI012\n"
-      #~ mock_accesseur.verify
+      @bde.lister_cours.must_equal "MAT002\nCHI005\nPHI012\n"
+      @mock_accesseur.verify
     end
   end
   
   describe "#selectionner_cours" do
     let(:cours_selectionne) { Cours.new( "PHI012" ) }
-    let(:liste_cours ) { [Cours.new( "MATH002"), \
-                          Cours.new( "CHIM005"), \
-                          Cours.new( "PHI012" )] }
-    before{ 
-      mock_accesseur = MiniTest::Mock.new
-      mock_accesseur.expect( :charger_base_donnee, \
-                              liste_cours, \
-                              [nom_fichier] )
-      @bde = BdCours.new( mock_accesseur ) }
-    
-    it "souleve une erreur si le sigle n'est pas au bon format" do
-      lambda{ @bde.selectionner_cours( "lapin" ) }.must_raise( RuntimeError )
-    end
+ 
+    #~ it "souleve une erreur si le sigle n'est pas au bon format" do
+      #~ lambda{ @bde.selectionner_cours( "lapin" ) }.must_raise( RuntimeError )
+    #~ end
     
     it "retourne nil si le cours n'existe pas" do
       @bde.charger_base_donnee( nom_fichier )
       @bde.selectionner_cours( "BLA005" ).must_equal( nil )
-      #~ mock_accesseur.verify
+      @mock_accesseur.verify
     end
     
     it "retourne le cours specifie par le sigle" do
       @bde.charger_base_donnee( nom_fichier )
-      @bde.selectionner_cours( "PHI012" ).sigle.must_equal( "PHI012" )
-      # Ce test ne me plait pas il faudrait implementer la methode ==
-      # pour voir faire une comparaison
-      #~ mock_accesseur.verify
+      ( @bde.selectionner_cours( "PHI012" ) == cours_selectionne ) == true
     end
   end    
 end
