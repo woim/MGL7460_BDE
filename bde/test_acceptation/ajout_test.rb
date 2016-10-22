31f5e4772741541136fb11396a01217c117e4998
francois <francoishebert@hotmail.fr>
2016-10-22 15:57:25 -0400

    Test acceptation ajout

--- bde/test_acceptation/ajout_test.rb
+++ bde/test_acceptation/ajout_test.rb
@@ -13,11 +13,18 @@ describe Bde do
   describe "ajout" do
     
     it "ajoute un cours sans sigle" do
-      bde_cli( 'ajout ' ).must_equal( ["Aucun sigle de cours saisit."] )
+      bde_cli( 'ajout' ).
+        must_equal( ["error: Le nom du cours doit etre de format SSSDDD"] )
     end
     
     it "ajoute un cours avec mauvais sigle" do
-      bde_cli( 'ajout DF54' ).must_equal( ["Aucun sigle de cours saisit."] )
+      bde_cli( 'ajout DF54' ).
+        must_equal( ["error: Le nom du cours doit etre de format SSSDDD"] )
+    end
+    
+    it "ajoute un cours qui existe deja" do
+      bde_cli( 'ajout -n MAT008' ).
+        must_equal( ["cours: MAT008 existe deja.\n"] )
     end
     
     it "ajoute un cours" do
@@ -28,18 +35,23 @@ describe Bde do
         "INF075/" ]
       bde_cli( 'ajout INF075' )
       File.open( nom_fichier, "r" ).read.
-        must_equal( nouvelle_base_donnee.join( "\n" ) 
+        must_equal( nouvelle_base_donnee.join( "\n" ) )
+    end
+    
+    it "ajoute un etudiant qui existe deja" do
+      bde_cli( '--class=MAT008 ajout -n MAT008' ).
+        must_equal( ["cours MAT008 / etudiant: Loiseau Martin existe deja.\n"] )
     end
     
     it "ajoute un etudiant" do
       nouvelle_base_donnee = [
-        "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17|Loup,Garou,Etienne",
+        "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17|Loup,Garou",
         "BIO012/Thibodeau,Jean,Charles-Henri=17,13,15|Loiseau,Martin=17,19,15",
         "PHY018/Loiseau,Martin=17,19,15|Thibodeau,Jean,Charles-Henri=18,19,16",
         "INF075/" ]
-      bde_cli( '--class=MAT008 ajout Loup Garou Etienne' )
+      bde_cli( '--class=MAT008 ajout -n Loup -p Garou' )
       File.open( nom_fichier, "r" ).read.
-        must_equal( nouvelle_base_donnee.join( "\n" )
+        must_equal( nouvelle_base_donnee.join( "\n" ) )
     end
 
   end
