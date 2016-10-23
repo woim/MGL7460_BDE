e6ef6a58418c50a565968efc1486b245f8b1057b
francois <francoishebert@hotmail.fr>
2016-10-23 11:37:35 -0400

    Test accpetatioin lister

--- bde/test_acceptation/lister_test.rb
+++ bde/test_acceptation/lister_test.rb
@@ -12,31 +12,35 @@ nom_fichier = ".bde.txt"
 
 describe Bde do
   describe "liste" do
-    before{ creer_base_donnee( base_donnee_test, nom_fichier ) }
     
     it "liste les classes" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
       bde_cli( 'liste' ).must_equal ['MAT008', 'BIO012', 'PHY018']
       effacer_base_donnee( nom_fichier )
     end
     
     it "liste les classes dans l'ordre alphabetique" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
       bde_cli( 'liste -a' ).must_equal ['BIO012', 'MAT008', 'PHY018']
       effacer_base_donnee( nom_fichier )
     end
 
     it "Envoie un message si le cours n'existe pas" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
       bde_cli( '--class=INF005 liste' ).
         must_equal ["cours: INF005 n'existe pas."]
       effacer_base_donnee( nom_fichier )  
     end
     
     it "liste les etudiants de la classe MAT008" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
       bde_cli( '--class=MAT008 liste' ).
         must_equal ['Loiseau Martin', 'Thibodeau Jean Charles-Henri']
       effacer_base_donnee( nom_fichier )
     end
     
     it "liste les etudiants de la classe BIO012 dans l'ordre alphabetique" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
       bde_cli( '--class=BIO012 liste -a' ).
         must_equal ['Loiseau Martin', 'Thibodeau Jean Charles-Henri']
       effacer_base_donnee( nom_fichier )
