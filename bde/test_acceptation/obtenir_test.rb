4357f05638ac3a7ac5cd8f39334a06d396582007
francois <francoishebert@hotmail.fr>
2016-10-23 12:03:55 -0400

    Test accetpation obtenir evaluation

---
+++ bde/test_acceptation/obtenir_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+require 'bde'
+
+
+# Cree une base de donnee locale pour test d'acceptation
+base_donnee_test = [
+"MAT008/Loiseau,Martin=12.0,13.0,14.0|Thibodeau,Jean,Charles-Henri=18.0,18.0,17.0",
+"BIO012/Thibodeau,Jean,Charles-Henri=17.0,13.0,15.0|Loiseau,Martin=17.0,19.0,15.0",
+"PHY018/Loiseau,Martin=17.0,19.0,15.0|Thibodeau,Jean,Charles-Henri=18.0,19.0,16.0" ]
+nom_fichier = ".bde.txt"
+
+
+describe Bde do
+  describe "obtenir_eval" do
+    before{ creer_base_donnee( base_donnee_test, nom_fichier ) }
+    
+    it "obtenir evaluation sans classe" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
+      bde_cli( 'obtenir_eval' ).
+        must_equal( ["Must select a class."] )
+      effacer_base_donnee( nom_fichier )
+    end
+    
+    it "obtenir evaluation si le cours n'existe pas" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
+      bde_cli( '--class=INF005 obtenir_eval' ).
+        must_equal ["cours: INF005 n'existe pas."]
+      effacer_base_donnee( nom_fichier )  
+    end
+    
+    it "obtenir evaluation sur un cours qui existe" do
+      creer_base_donnee( base_donnee_test, nom_fichier )
+      bde_cli( '--class=MAT008 obtenir_eval' ).
+        must_equal( [ "Loiseau Martin: 12.0 13.0 14.0",
+                      "Thibodeau Jean Charles-Henri: 18.0 18.0 17.0"] )
+      effacer_base_donnee( nom_fichier )  
+    end
+   
+  end
+end
