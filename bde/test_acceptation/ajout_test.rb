9360b15c10aca2491b08e18a0d6a2219f61fb0b5
francois <francoishebert@hotmail.fr>
2016-10-23 07:48:20 -0400

    Commande est un verbe

--- bde/test_acceptation/ajout_test.rb
+++ bde/test_acceptation/ajout_test.rb
@@ -1,34 +1,38 @@
 require 'test_helper'
 require 'bde'
 
+
 # Cree une base de donnee locale pour test d'acceptation
 base_donnee_test = [
 "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17",
 "BIO012/Thibodeau,Jean,Charles-Henri=17,13,15|Loiseau,Martin=17,19,15",
 "PHY018/Loiseau,Martin=17,19,15|Thibodeau,Jean,Charles-Henri=18,19,16" ]
 nom_fichier = ".bde.txt"
-#creer_base_donnee( base_donnee_test, nom_fichier )
+
 
 describe Bde do
-  describe "ajout" do
+  describe "ajouter" do
     before{ creer_base_donnee( base_donnee_test, nom_fichier ) }
     
-    it "ajoute un cours sans sigle" do
+    it "ajouter un cours sans sigle" do
       bde_cli( 'ajout' ).
         must_equal( ["error: Le nom du cours doit etre de format SSSDDD"] )
+      effacer_base_donnee( nom_fichier )
     end
     
-    it "ajoute un cours avec mauvais sigle" do
+    it "ajouter un cours avec mauvais sigle" do
       bde_cli( 'ajout -n DF54' ).
         must_equal( ["error: Le nom du cours doit etre de format SSSDDD"] )
+      effacer_base_donnee( nom_fichier )
     end
     
-    it "ajoute un cours qui existe deja" do
+    it "ajouter un cours qui existe deja" do
       bde_cli( 'ajout -n MAT008' ).
         must_equal( ["error: MAT008 existe deja."] )
+      effacer_base_donnee( nom_fichier )
     end
     
-    it "ajoute un cours" do
+    it "ajouter un cours" do
       nouvelle_base_donnee = [
         "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17",
         "BIO012/Thibodeau,Jean,Charles-Henri=17,13,15|Loiseau,Martin=17,19,15",
@@ -40,12 +44,13 @@ describe Bde do
       effacer_base_donnee( nom_fichier )
     end
     
-    it "ajoute un etudiant qui existe deja" do
+    it "ajouter un etudiant qui existe deja" do
       bde_cli( '--class=MAT008 ajout -n Loiseau -p Martin' ).
         must_equal( ["error: Loiseau Martin existe deja."] )
+      effacer_base_donnee( nom_fichier )
     end
     
-    it "ajoute un etudiant" do
+    it "ajouter un etudiant" do
       nouvelle_base_donnee = [
         "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17|Loup,Garou",
         "BIO012/Thibodeau,Jean,Charles-Henri=17,13,15|Loiseau,Martin=17,19,15",
--- bde/test_acceptation/liste_test.rb
+++ bde/test_acceptation/liste_test.rb
@@ -1,42 +1,46 @@
 require 'test_helper'
 require 'bde'
 
+
 # Cree une base de donnee locale pour test d'acceptation
 base_donnee_test = [
 "MAT008/Loiseau,Martin=12,13,14|Thibodeau,Jean,Charles-Henri=18,18,17",
 "BIO012/Thibodeau,Jean,Charles-Henri=17,13,15|Loiseau,Martin=17,19,15",
 "PHY018/Loiseau,Martin=17,19,15|Thibodeau,Jean,Charles-Henri=18,19,16" ]
 nom_fichier = ".bde.txt"
-creer_base_donnee( base_donnee_test, nom_fichier )
+
 
 describe Bde do
   describe "liste" do
+    before{ creer_base_donnee( base_donnee_test, nom_fichier ) }
     
     it "liste les classes" do
       bde_cli( 'liste' ).must_equal ['MAT008', 'BIO012', 'PHY018']
+      effacer_base_donnee( nom_fichier )
     end
     
     it "liste les classes dans l'ordre alphabetique" do
       bde_cli( 'liste -a' ).must_equal ['BIO012', 'MAT008', 'PHY018']
+      effacer_base_donnee( nom_fichier )
     end
 
     it "Envoie un message si le cours n'existe pas" do
       bde_cli( '--class=INF005 liste' ).
         must_equal ["cours: INF005 n'existe pas."]
+      effacer_base_donnee( nom_fichier )  
     end
     
     it "liste les etudiants de la classe MAT008" do
       bde_cli( '--class=MAT008 liste' ).
         must_equal ['Loiseau Martin', 'Thibodeau Jean Charles-Henri']
+      effacer_base_donnee( nom_fichier )
     end
     
     it "liste les etudiants de la classe BIO012 dans l'ordre alphabetique" do
       bde_cli( '--class=BIO012 liste -a' ).
         must_equal ['Loiseau Martin', 'Thibodeau Jean Charles-Henri']
+      effacer_base_donnee( nom_fichier )
     end
     
   end
 end
-
-# Efface la base de donnee
-#effacer_base_donnee( nom_fichier )
