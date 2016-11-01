a0a5bbe258499a28cd5ec955aefc967e4dd0c6e3
Élodie Marteau <elodiemarteau@MacBook-Pro-de-Elodie.local>
2016-11-01 12:22:38 -0400

    Modification test_cours pour verifier_cours

--- bde/lib/bde/fonction.rb
+++ bde/lib/bde/fonction.rb
@@ -1,4 +1,4 @@
-def test_cours( sigle, base_donnee )
+def verifier_cours( sigle, base_donnee )
   fail "Un sigle de cours doit etre fourni." if sigle.nil?
   fail "Cours " + sigle + " n\'existe pas.\n" if !base_donnee.cours_existe?( sigle )
 end
